ea7015f0932ea236cf456c26130d910fa62a8710
faris-code0317 <farissmk@gmail.com>
2025-05-07 08:50:04 +0700

    Update routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\bukuController;
 
 Route::get('/', [bukuController::class, 'index']);
+Route::post('/tambahBuku', [bukuController::class, 'add']);
+Route::delete('/deleteBuku{id}', [bukuController::class, 'destroy']);
+Route::get('/edit{id}', [bukuController::class, 'edit']);
+Route::put('/updateBuku/{id}', [bukuController::class, 'update']);
